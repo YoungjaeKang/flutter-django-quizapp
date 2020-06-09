28bfbb5188468898b3a5aa9ec329877389785f6c
yjk <kyj8028@hotmail.com>
2020-06-09 23:04:04 +0900

    #2.0 DRF Coding

---
+++ quiz_backend_test/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/toypanda/Documents/GitHub/flutter-django-quizapp/quiz_backend_test/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
